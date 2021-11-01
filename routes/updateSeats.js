3940d1f7f59b38c61fe919ad79ffdc140959638e
Dipanshu Gupta <dipanshug124@gmail.com>
2021-11-01 17:35:28 +0530

    updated api

--- routes/updateSeats.js
+++ routes/updateSeats.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 const path = require("path");
 const Seat = require("../models/seat");
 
-router.patch("/:seatId", async (req, res) => {
+router.patch("/:seatNo", async (req, res) => {
   try {
     const updatedSeat = await Seat.updateOne(
-      { _id: req.params.seatId },
+      { seatNo: req.params.seatNo },
       { $set: { booked: true } }
     );
     res.json(updatedSeat);
